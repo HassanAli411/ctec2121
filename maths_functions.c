f977d64a665bd8b99223341e65a19cda0bf7314c
Hassan Ali <p16152408@dmu.ac.uk>
2018-01-22 13:28:27 +0000

    maths_function subtract

--- maths_functions.c
+++ maths_functions.c
@@ -5,7 +5,7 @@ int main()
 int a=0,b=0,sum=0;    		
 printf("Enter two numbers..."); 
 scanf("%d%d",&a,&b);      
-sum=a+b;                   
+sum=a-b;                   
 printf("The sum is %d",sum); 
 return 0;
 }
