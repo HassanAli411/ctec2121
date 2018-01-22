657079abfec8992af41c5870febf042cc70a4af3
Hassan Ali <p16152408@dmu.ac.uk>
2018-01-22 13:41:32 +0000

    now including forgotten.txt

--- maths_functions.c
+++ maths_functions.c
@@ -3,9 +3,9 @@
 int main()
 {
 int a=0,b=0,sum=0;    		
-printf("Enter two numbers..."); 
+printf("Enter two numbers with min being 1 and max 5..."); 
 scanf("%d%d",&a,&b);      
-sum=a*b;                   
-printf("The sum is %d",sum); 
+sum=(a+b)/2;                   
+printf("The average is %d",sum); 
 return 0;
 }
