fc285a5eae9dd0220768ab6292af7534144efe96
Hassan <p16152408@my365.dmu.ac.uk>
2018-01-29 13:42:22 +0000

    maths function remainder

--- maths_functions.c
+++ maths_functions.c
@@ -5,7 +5,7 @@ int main()
 int a=0,b=0,sum=0;    		
 printf("Enter two numbers with min being 1 and max 5..."); 
 scanf("%d%d",&a,&b);      
-sum=(a+b)/2;                   
-printf("The average is %d",sum); 
+sum=a-b;                   
+printf("The remainder is %d",sum); 
 return 0;
 }
